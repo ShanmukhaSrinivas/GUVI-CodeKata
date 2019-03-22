762e6116f1d0c98666d753c87d205ea6f02ac6aa
ShanmukhaSrinivas <vemulasrinivas2505@gmail.com>
2019-03-22 19:25:46 +0530

    Problem#101

--- Beginner/Session-11/problem#101.py
+++ Beginner/Session-11/problem#101.py
@@ -1,8 +1,15 @@
+def find(n,m):
+    l = []
+    assert len(n)<=m
+    for i in range(1,m+1):
+        l.append(n[-i])
+    l.reverse()
+    [print(i,end='') for i in l]
+    
 n, m = [i for i in input().split()]
 n = list(n)
 m = int(m)
-l = []
-for i in range(1,m+1):
-    l.append(n[-i])
-l.reverse()
-[print(i,end='') for i in l]
+try:
+    find(n,m)
+except:
+    print('Invalid Input')
