a4d7c9fbce3e611bcbe6926652a5fc70a8042381
ShanmukhaSrinivas <vemulasrinivas2505@gmail.com>
2019-03-24 19:24:32 +0530

    Problem#103

--- Beginner/Session-11/problem#103.py
+++ Beginner/Session-11/problem#103.py
@@ -3,5 +3,5 @@ n = [i for i in input().split()]
 l = []
 for i in n:
     l.append(i[0].upper()+i[1:])
-[print(l[i],end="$") for i in range(len(l)-1)]
+[print(l[i],end=" ") for i in range(len(l)-1)]
 print(l[len(l)-1])
