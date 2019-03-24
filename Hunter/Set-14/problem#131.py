635f08f6b3ea79bdbaf08297f3f92f57466535a0
ShanmukhaSrinivas <vemulasrinivas2505@gmail.com>
2019-03-24 19:23:22 +0530

    Problem#103

--- Hunter/Set-14/problem#131.py
+++ Hunter/Set-14/problem#131.py
@@ -2,4 +2,4 @@
 y = int(input())
 x = [int(i) for i in input().split()]
 x = sorted(x)
-[print(x[-(i+1)],x[i],end = " ") for i in range(int(len(x)/2))]
+[print(x[-(i+1)],x[i],end = " ") for i in range(int(len(x)+1/2))]
