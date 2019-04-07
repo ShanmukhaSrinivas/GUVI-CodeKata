c6b969ebbfe1c51b51879d41d4c5631b3594639d
Vemula.Shanmukha Srinivas <36005710+ShanmukhaSrinivas@users.noreply.github.com>
2019-04-07 16:33:36 +0530

    Create problem#11.py

---
+++ Player/Set- 2/problem#11.py
@@ -0,0 +1,10 @@
+#weekend
+x = input()
+weekend = ["Saturday","Sunday"]
+weekday = ["Monday","Tuesday","Wednesday","Thursday","Friday"]
+if x in weekend:
+    print("yes")
+elif x in weekday:
+    print("no")
+else:
+    print("Invalid Input")
