d747fa3b58c9c49b07d535d14a6c6407568ae34d
ShanmukhaSrinivas <vemulasrinivas2505@gmail.com>
2019-04-07 20:12:08 +0530

    Problem#21

--- Player/Set-3/problem#21.py
+++ Player/Set-3/problem#21.py
@@ -2,7 +2,7 @@
 x = [int(i) for i in input().split()]
 y = [int(i) for i in input().split()]
 z = [int(i) for i in input().split()]
-if x[0]==y[0]==z[0] or x[1]==y[1]==z[1]:
+if (x[0]*(y[1]-z[1]))+(y[0]*(z[1]-x[1]))+(z[0]*(x[1]-y[1])) == 0:
     print("yes")
 else:
     print("no")
