8261763b17a2b916fc4ebd4cb24105b7b6c309f0
ShanmukhaSrinivas <vemulasrinivas2505@gmail.com>
2019-04-16 20:19:13 +0530

    Problem#02

--- Beginner/Pro/Set-01/problem#02.py
+++ Beginner/Pro/Set-01/problem#02.py
@@ -1,7 +1,7 @@
-from itertools import permutations
+from itertools import combinations
 x, y = [i for i in input().split()]
 l=[]
-per = permutations(x,len(x)-int(y))
+per = combinations(x,len(x)-int(y))
 for i in per:
     l.append(int(''.join(list(i))))
 print(min(l))
