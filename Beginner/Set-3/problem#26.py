61983f04fef68cf8adbe1d1d0e6a43eaed2e007f
Vemula.Shanmukha Srinivas <36005710+ShanmukhaSrinivas@users.noreply.github.com>
2019-04-23 13:30:53 +0530

    Problem#26

--- Beginner/Set-3/problem#26.py
+++ Beginner/Set-3/problem#26.py
@@ -1,5 +1,5 @@
 #Sort given data
-m = int(input())
-l = [int(i) for i in input().split()]
-l.sort()
-print(l)
+a = [int(i) for i in input().split()]
+a.sort()
+a = [str(i) for i in a]
+print(' '.join(a))
