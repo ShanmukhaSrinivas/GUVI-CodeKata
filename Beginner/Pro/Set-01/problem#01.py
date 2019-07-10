024c59b43c986a12005c7e1a2cea8697e056818c
ShanmukhaSrinivas <vemulasrinivas2505@gmail.com>
2019-07-10 10:23:25 +0530

    Problem#01

--- Beginner/Pro/Set-01/problem#01.py
+++ Beginner/Pro/Set-01/problem#01.py
@@ -1,11 +1,13 @@
-#Max common element in the input
 x = []
-l = []
 n = int(input())
 for i in range(n):
     x.append(input())
-for i in range(min(len(x[0]),len(x[1]))):
-    if x[0][i] == x[1][i]:
-            l.append(x[0][i])
+k = x[0]
+for i in x[1:]:
+    l = []
+    for j in range(min(len(k), len(i))):
+        if k[j] == i[j]:
+            l.append(k[j])
+    k = ''.join(l)
 
-print(l)
+print(k)
