df7f2b0b35100b44ec5f25ffdc7ac9de29075e97
ShanmukhaSrinivas <vemulasrinivas2505@gmail.com>
2019-07-10 09:19:45 +0530

    Problem#63

--- Beginner/Pro/Set-07/problem#63.py
+++ Beginner/Pro/Set-07/problem#63.py
@@ -2,11 +2,11 @@ def func(string):
     k = ''
     LengthOfPalindrome = len(string)
     for i in range(LengthOfPalindrome+1):
-        for j in range(LengthOfPalindrome+1):
-            new = string[i:j]
-            if len(new) == len(set(new)):
-                if len(k) < len(new):
-                    k = new
+        new = string[0:i]
+        print(new)
+        if len(new) == len(set(new)):
+            if len(k) < len(new):
+                k = new
     return len(k)
 
 
